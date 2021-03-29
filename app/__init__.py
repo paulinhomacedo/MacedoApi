92bde66853270bc55f8c1f330682387b906dcae7
paulinhomacedo <macedopham@gmail.com>
2021-03-29 12:13:26 -0300

    .

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,13 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    
-    from app import route
 
-    route.init_app(app)   
+    @app.route('/')
+    def hello_world():
+        return 'Hello, World!'
+    
+    '''from app import route
+
+    route.init_app(app) '''  
 
     return app
