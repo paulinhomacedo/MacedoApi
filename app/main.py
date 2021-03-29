5d85833a78d78dd3d2aa4b6b4b876ff70ce3001d
paulinhomacedo <macedopham@gmail.com>
2021-03-29 16:59:56 -0300

    cotacao moeda

--- app/main.py
+++ app/main.py
@@ -12,3 +12,13 @@ async def root():
 def busca_cep(cep_id  = '05145901'):   
     response = get(f'https://cep.awesomeapi.com.br/{cep_id}').json()
     return response  
+
+@app.get("/cotacao-moeda")
+def cotacao_all():
+    response = get('https://economia.awesomeapi.com.br/json/all').json()
+    return response
+
+@app.get("/cotacao-moeda/{id_moeda}")
+def cotacao_moeda(id_moeda):
+    response = get(f'https://economia.awesomeapi.com.br/json/all/{id_moeda}').json()
+    return response
